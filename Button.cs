d262b04c77b17cd41d19782498cf3a9073b77fb1
Konstantin Konstantin <zetslief@protonmail.com>
2025-11-17 22:13:45 +0100

    ShuffleContainer: move shuffle logic from Button

--- Button.cs
+++ Button.cs
@@ -3,36 +3,13 @@ using System;
 
 public partial class Button : Godot.Button
 {
-    private Vector2 _viewportSize;
+    [Export]
+    public SpriteContainer SpriteContainer { get; set; } = default!;
 
     public override void _Ready()
     {
-        _viewportSize = GetViewportRect().Size;
-        _Pressed();
+        if (SpriteContainer is null) throw new InvalidOperationException($"SpriteContainer is not initialized.");
     }
 
-    public override void _Pressed()
-    {
-        var spriteContainer = GetNode("/root/Main/SpriteContainer")
-            ?? throw new InvalidOperationException($"Failed to find sprite container node.");;
-        foreach (var child in spriteContainer.GetChildren())
-        {
-            if (child is TrashImage sprite)
-            {
-                var resultSize = TrashImage.SetSize(sprite, 204);
-                var standardOffset = new Vector2(_viewportSize.X * 0.01f, _viewportSize.Y * 0.05f);
-                var targetPosition = CalculateRandomPosition(_viewportSize, standardOffset, resultSize);
-                var tween = CreateTween();
-                tween.TweenProperty(child, (string)Node2D.PropertyName.Position, targetPosition, 3)
-                    .SetEase(Tween.EaseType.In)
-                    .SetTrans(Tween.TransitionType.Linear);
-            }
-        }
-    }
-
-    private static Vector2 CalculateRandomPosition(Vector2 viewportSize, Vector2 defaultOffset, Vector2 rightBottomOffset)
-    {
-        var random = new Vector2(Random.Shared.NextSingle(), Random.Shared.NextSingle());
-        return defaultOffset + random * (viewportSize - defaultOffset * 2f - rightBottomOffset);
-    }
+    public override void _Pressed() => SpriteContainer.Shuffle(SpriteContainer);
 }
--- SpriteContainer.cs
+++ SpriteContainer.cs
@@ -4,12 +4,15 @@ using System.Linq;
 
 public partial class SpriteContainer : Node2D
 {
+    private Vector2 _viewportSize;
+
     [Export]
     public PackedScene TrashImageScene { get; set; } = default!;
 
     public override void _Ready()
     {
         if (TrashImageScene is null) throw new InvalidOperationException($"Trash image scene is not set.");
+        _viewportSize = GetViewportRect().Size;
         var textures = Enumerable.Range(1, 14).Select(i => (i, GD.Load<Texture2D>($"res://images/trash_{i}.png")));
         foreach (var (index, texture) in textures)
         {
@@ -18,10 +21,29 @@ public partial class SpriteContainer : Node2D
             node.Texture = texture;
             AddChild(node);
         }
+        Shuffle(this);
     }
 
-    // Called every frame. 'delta' is the elapsed time since the previous frame.
-    public override void _Process(double delta)
+    public static void Shuffle(SpriteContainer container)
     {
+        foreach (var child in container.GetChildren())
+        {
+            if (child is TrashImage sprite)
+            {
+                var resultSize = TrashImage.SetSize(sprite, 204);
+                var standardOffset = new Vector2(container._viewportSize.X * 0.01f, container._viewportSize.Y * 0.05f);
+                var targetPosition = CalculateRandomPosition(container._viewportSize, standardOffset, resultSize);
+                var tween = container.CreateTween();
+                tween.TweenProperty(child, (string)Node2D.PropertyName.Position, targetPosition, 3)
+                    .SetEase(Tween.EaseType.In)
+                    .SetTrans(Tween.TransitionType.Linear);
+            }
+        }
+    }
+
+    private static Vector2 CalculateRandomPosition(Vector2 viewportSize, Vector2 defaultOffset, Vector2 rightBottomOffset)
+    {
+        var random = new Vector2(Random.Shared.NextSingle(), Random.Shared.NextSingle());
+        return defaultOffset + random * (viewportSize - defaultOffset * 2f - rightBottomOffset);
     }
 }
